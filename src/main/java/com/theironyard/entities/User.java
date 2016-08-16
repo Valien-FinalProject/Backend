303d3f1ee1a4cc366eca38ba1308d4bc0398a4ef
Eddy Jimenez <EddyJ@Eduardos-MacBook-Pro.local>
2016-08-16 15:54:49 -0700

    fixing username so now its unique

--- src/main/java/com/theironyard/entities/User.java
+++ src/main/java/com/theironyard/entities/User.java
@@ -29,7 +29,7 @@ public abstract class User {
     @Column
     protected String email;
 
-    @Column
+    @Column(nullable = false, unique = true)
     protected String username;
 
     @Column(nullable = false)
