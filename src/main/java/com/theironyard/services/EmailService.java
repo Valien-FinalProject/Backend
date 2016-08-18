6d2e8da3d8c2a6f2f5606cf98188ea8251cfc176
NR44 <darkbrilliance@gmail.com>
2016-08-18 10:00:19 -0700

    added base code for SendGrid. adjusted procfile.

--- src/main/java/com/theironyard/services/EmailService.java
+++ src/main/java/com/theironyard/services/EmailService.java
@@ -1,5 +1,8 @@
 package com.theironyard.services;
 
+import com.sendgrid.Content;
+import com.sendgrid.Email;
+import com.sendgrid.Mail;
 import com.sendgrid.SendGrid;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +14,11 @@ import org.springframework.stereotype.Service;
 public class EmailService {
 
     public void sendEmail(String name, String email){
+        Email from = new Email("test@example.com");
+        String subject = "Hello World from the SendGrid Java Library!";
+        Email to = new Email("test@example.com");
+        Content content = new Content("text/plain", "Hello, Email!");
+        Mail mail = new Mail(from, subject, to, content);
 
         SendGrid sendGrid = new SendGrid(System.getenv("SENDGRID_API_KEY"));
 
