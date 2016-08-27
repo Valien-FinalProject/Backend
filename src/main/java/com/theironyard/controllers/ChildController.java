9c7326bc5fb8e282aac8e51a295b88c73dbce792
NR44 <darkbrilliance@gmail.com>
2016-08-27 15:19:25 -0700

    attempted to fix duplicates in child/current

--- src/main/java/com/theironyard/controllers/ChildController.java
+++ src/main/java/com/theironyard/controllers/ChildController.java
@@ -160,12 +160,10 @@ public class ChildController {
     @RequestMapping(path = "/current", method = RequestMethod.GET)
     public Collection<Chore> getCurrentChore(@RequestHeader(value = "Authorization") String token){
         Child child = authService.getChildFromAuth(token);
-        Parent parent = child.getParent();
 
-        Collection<Chore> parentChoreCollection = parent.getChoreCollection();
         Collection<Chore> currentChoreList = new ArrayList<>();
 
-        parentChoreCollection.stream().filter(chore -> chore.isPending() == false && chore.isComplete() == false && chore.getChildAssigned() == child).forEach(chore -> currentChoreList.add(chore));
+        child.getChoreCollection().stream().filter(chore -> !chore.isPending()&& !chore.isComplete() && chore.getChildAssigned() == child).forEach(chore -> currentChoreList.add(chore));
 
         return currentChoreList;
     }
